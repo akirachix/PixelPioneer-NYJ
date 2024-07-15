3bead371d77d2dd680f7cb4cd4661e693993fc6f
Jemimah-Nthenya <jemimahogonda@gmail.com>
2024-07-15 06:53:08 +0300

    add work

--- nyj/src/Video/index.js
+++ nyj/src/Video/index.js
@@ -7,8 +7,8 @@ const Video = () => {
         <h1 className="video">Videos</h1>
           <div id="embark">
         <h1  className='text' ><span>Embark on a multimedia journey with our newest videos and get to learn how WiFi works</span></h1>
-        <p className="text2">Wifi the magic that lets you magic you surf the internet  from<br></br>your couch,
-       without having to get up and plug in a cord.<br></br>It's like having a personal genie that grants you<br></br>internet accessfrom anywhere in your home.</p>
+        <p className="text2">Wifi the magic that lets you magic you surf the internet  from your couch,
+       without having to get up and plug in a cord. It's like having a personal genie that grants you internet accessfrom anywhere in your home.</p>
        </div>
         <div className='pagetext'>
        <iframe  src="https://www.loom.com/embed/0b2b73df9edd4fca9f1efc4c3efcc6b8?sid=ba66d772-c08f-4fc4-8992-11845aa1acc5" 
